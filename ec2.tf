b82f8073ec8883980d547ebce5707346b18a43b5
saitej-12 <saitejakancharla1612@gmail.com>
2025-10-26 20:34:57 +0530

    first

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,32 @@
 resource "aws_instance" "example" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
-
+  vpc_security_group_ids =[aws_security_group.allow_alls.id]
   tags = {
     Name = "HelloWorld"
+    Terraform="true"
+  }
+}
+
+resource "aws_security_group" "allow_alls" {
+  name   = "allow_alls"
+
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+  tags= {
+    Name = "allow_alls"
   }
 }
